2eaec08fab9c004edf7053b5cb12bf6c49dc075e
lijandy92 <118372263+lijandy92@users.noreply.github.com>
2023-06-22 05:43:00 -1200

    aaa
    aaaa

--- ising.c
+++ ising.c
@@ -11,37 +11,55 @@ void update(const float temp, int grid[L][L])
 {
     for (unsigned int i = 0; i < L; ++i) {
         for (unsigned int j = 0; j < L; j += 4) {
-            // Load data into SSE registers
-            __m128i spin_old = _mm_loadu_si128((__m128i*)&grid[i][j]);
-            __m128i spin_new = _mm_sub_epi32(_mm_set1_epi32(-1), spin_old);
+            int spin_old[4] = { grid[i][j], grid[i][j + 1], grid[i][j + 2], grid[i][j + 3] };
+            int spin_new[4] = { -spin_old[0], -spin_old[1], -spin_old[2], -spin_old[3] };
 
-            // Compute energy contributions of neighbors
-            __m128i spin_neigh_n = _mm_loadu_si128((__m128i*)&grid[(i + 1) % L][j]);
-            __m128i spin_neigh_e = _mm_loadu_si128((__m128i*)&grid[i][(j + 1) % L]);
-            __m128i spin_neigh_w = _mm_loadu_si128((__m128i*)&grid[i][(j + L - 1) % L]);
-            __m128i spin_neigh_s = _mm_loadu_si128((__m128i*)&grid[(i + L - 1) % L][j]);
+            // computing energy contributions of neighbors
+            int spin_neigh_n[4] = { grid[(i + L - 1) % L][j], grid[(i + L - 1) % L][j + 1],
+                                    grid[(i + L - 1) % L][j + 2], grid[(i + L - 1) % L][j + 3] };
+            int spin_neigh_e[4] = { grid[i][(j + 1) % L], grid[i][(j + 2) % L],
+                                    grid[i][(j + 3) % L], grid[i][(j + 4) % L] };
+            int spin_neigh_w[4] = { grid[i][(j + L - 1) % L], grid[i][(j + L - 2) % L],
+                                    grid[i][(j + L - 3) % L], grid[i][(j + L - 4) % L] };
+            int spin_neigh_s[4] = { grid[(i + 1) % L][j], grid[(i + 1) % L][j + 1],
+                                    grid[(i + 1) % L][j + 2], grid[(i + 1) % L][j + 3] };
+            
+            int delta_spin_neigh_n[4] = { spin_neigh_n[0] - spin_old[0], spin_neigh_n[1] - spin_old[1],
+                                          spin_neigh_n[2] - spin_old[2], spin_neigh_n[3] - spin_old[3] };
+            int delta_spin_neigh_e[4] = { spin_neigh_e[0] - spin_old[0], spin_neigh_e[1] - spin_old[1],
+                                          spin_neigh_e[2] - spin_old[2], spin_neigh_e[3] - spin_old[3] };
+            int delta_spin_neigh_w[4] = { spin_neigh_w[0] - spin_old[0], spin_neigh_w[1] - spin_old[1],
+                                          spin_neigh_w[2] - spin_old[2], spin_neigh_w[3] - spin_old[3] };
+            int delta_spin_neigh_s[4] = { spin_neigh_s[0] - spin_old[0], spin_neigh_s[1] - spin_old[1],
+                                          spin_neigh_s[2] - spin_old[2], spin_neigh_s[3] - spin_old[3] };
+            
+            // computing h_before and h_after
+            int h_before[4] = { -spin_old[0] * (delta_spin_neigh_n[0] + delta_spin_neigh_e[0] + delta_spin_neigh_w[0] + delta_spin_neigh_s[0]),
+                                -spin_old[1] * (delta_spin_neigh_n[1] + delta_spin_neigh_e[1] + delta_spin_neigh_w[1] + delta_spin_neigh_s[1]),
+                                -spin_old[2] * (delta_spin_neigh_n[2] + delta_spin_neigh_e[2] + delta_spin_neigh_w[2] + delta_spin_neigh_s[2]),
+                                -spin_old[3] * (delta_spin_neigh_n[3] + delta_spin_neigh_e[3] + delta_spin_neigh_w[3] + delta_spin_neigh_s[3]) };
+            int h_after[4] = { -spin_new[0] * (delta_spin_neigh_n[0] + delta_spin_neigh_e[0] + delta_spin_neigh_w[0] + delta_spin_neigh_s[0]),
+                               -spin_new[1] * (delta_spin_neigh_n[1] + delta_spin_neigh_e[1] + delta_spin_neigh_w[1] + delta_spin_neigh_s[1]),
+                               -spin_new[2] * (delta_spin_neigh_n[2] + delta_spin_neigh_e[2] + delta_spin_neigh_w[2] + delta_spin_neigh_s[2]),
+                               -spin_new[3] * (delta_spin_neigh_n[3] + delta_spin_neigh_e[3] + delta_spin_neigh_w[3] + delta_spin_neigh_s[3]) };
 
-            __m128i delta_spin_neigh_n = _mm_sub_epi32(spin_neigh_n, spin_old);
-            __m128i delta_spin_neigh_e = _mm_sub_epi32(spin_neigh_e, spin_old);
-            __m128i delta_spin_neigh_w = _mm_sub_epi32(spin_neigh_w, spin_old);
-            __m128i delta_spin_neigh_s = _mm_sub_epi32(spin_neigh_s, spin_old);
+            int delta_E[4] = { h_after[0] - h_before[0], h_after[1] - h_before[1],
+                               h_after[2] - h_before[2], h_after[3] - h_before[3] };
 
-            // Compute h_before and h_after
-            __m128i h_before = _mm_add_epi32(_mm_add_epi32(delta_spin_neigh_n, delta_spin_neigh_e),
-                                              _mm_add_epi32(delta_spin_neigh_w, delta_spin_neigh_s));
-            __m128i h_after = _mm_sub_epi32(h_before, _mm_slli_epi32(spin_new, 2));
-
-            __m128i delta_E = _mm_sub_epi32(h_after, h_before);
-            __m128 p = _mm_div_ps(_mm_cvtepi32_ps(_mm_set1_epi32(rand())), _mm_set1_ps(RAND_MAX));
+            // Generate random values
+            float rand_vals[4] = { rand() / (float)RAND_MAX, rand() / (float)RAND_MAX,
+                                   rand() / (float)RAND_MAX, rand() / (float)RAND_MAX };
 
             // Compare delta_E with zero and p with exp(-delta_E / temp)
             __m128 accept_mask = _mm_or_ps(_mm_cmple_ps(_mm_setzero_ps(), _mm_cvtepi32_ps(delta_E)),
-                                            _mm_cmple_ps(p, _mm_exp_ps(_mm_div_ps(_mm_cvtepi32_ps(delta_E), _mm_set1_ps(temp)))));
+                                           _mm_cmple_ps(_mm_set_ps(rand_vals[3], rand_vals[2], rand_vals[1], rand_vals[0]),
+                                                        _mm_div_ps(_mm_cvtepi32_ps(delta_E), _mm_set1_ps(temp))));
 
-            // Blend spin_old and spin_new based on accept_mask
-            __m128i result = _mm_blendv_epi8(spin_old, spin_new, _mm_castps_si128(accept_mask));
+            // Blend spin_old and spin_new based on the accept_mask
+            __m128i result = _mm_blendv_epi32(_mm_loadu_si128((__m128i*)spin_old),
+                                               _mm_loadu_si128((__m128i*)spin_new), accept_mask);
 
-            // Store the result back to memory
+            // Store the updated spins back to the grid
             _mm_storeu_si128((__m128i*)&grid[i][j], result);
         }
     }
