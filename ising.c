0b7cd1e39f40ebebddf3f55eb90af2f86dbee383
lijandy92 <118372263+lijandy92@users.noreply.github.com>
2023-06-22 05:36:22 -1200

    SSE
    SSE

--- ising.c
+++ ising.c
@@ -5,42 +5,48 @@
 #include <immintrin.h>
 
 //Modificada Comentada
+#include <xmmintrin.h> // SSE intrinsics
+
 void update(const float temp, int grid[L][L])
 {
-    // typewriter update
     for (unsigned int i = 0; i < L; ++i) {
-        for (unsigned int j = 0; j < L; j += 8) {
-            __m256i spin_old = _mm256_loadu_si256((__m256i*)&grid[i][j]);
-            __m256i spin_new = _mm256_sub_epi32(_mm256_set1_epi32(-1), spin_old);
+        for (unsigned int j = 0; j < L; j += 4) {
+            // Load data into SSE registers
+            __m128i spin_old = _mm_loadu_si128((__m128i*)&grid[i][j]);
+            __m128i spin_new = _mm_sub_epi32(_mm_set1_epi32(-1), spin_old);
 
-            // computing energy contributions of neighbors
-            __m256i spin_neigh_n = _mm256_loadu_si256((__m256i*)&grid[(i + L - 1) % L][j]);
-            __m256i spin_neigh_e = _mm256_loadu_si256((__m256i*)&grid[i][(j + 1) % L]);
-            __m256i spin_neigh_w = _mm256_loadu_si256((__m256i*)&grid[i][(j + L - 1) % L]);
-            __m256i spin_neigh_s = _mm256_loadu_si256((__m256i*)&grid[(i + 1) % L][j]);
+            // Compute energy contributions of neighbors
+            __m128i spin_neigh_n = _mm_loadu_si128((__m128i*)&grid[(i + 1) % L][j]);
+            __m128i spin_neigh_e = _mm_loadu_si128((__m128i*)&grid[i][(j + 1) % L]);
+            __m128i spin_neigh_w = _mm_loadu_si128((__m128i*)&grid[i][(j + L - 1) % L]);
+            __m128i spin_neigh_s = _mm_loadu_si128((__m128i*)&grid[(i + L - 1) % L][j]);
 
-            __m256i delta_spin_neigh_n = _mm256_sub_epi32(spin_neigh_n, spin_old);
-            __m256i delta_spin_neigh_e = _mm256_sub_epi32(spin_neigh_e, spin_old);
-            __m256i delta_spin_neigh_w = _mm256_sub_epi32(spin_neigh_w, spin_old);
-            __m256i delta_spin_neigh_s = _mm256_sub_epi32(spin_neigh_s, spin_old);
+            __m128i delta_spin_neigh_n = _mm_sub_epi32(spin_neigh_n, spin_old);
+            __m128i delta_spin_neigh_e = _mm_sub_epi32(spin_neigh_e, spin_old);
+            __m128i delta_spin_neigh_w = _mm_sub_epi32(spin_neigh_w, spin_old);
+            __m128i delta_spin_neigh_s = _mm_sub_epi32(spin_neigh_s, spin_old);
 
-            // computing h_before and h_after
-            __m256i h_before = _mm256_mul_epi32(spin_old, _mm256_add_epi32(_mm256_add_epi32(delta_spin_neigh_n, delta_spin_neigh_e),
-                _mm256_add_epi32(delta_spin_neigh_w, delta_spin_neigh_s)));
-            __m256i h_after = _mm256_mul_epi32(spin_new, _mm256_add_epi32(_mm256_add_epi32(delta_spin_neigh_n, delta_spin_neigh_e),
-                _mm256_add_epi32(delta_spin_neigh_w, delta_spin_neigh_s)));
+            // Compute h_before and h_after
+            __m128i h_before = _mm_add_epi32(_mm_add_epi32(delta_spin_neigh_n, delta_spin_neigh_e),
+                                              _mm_add_epi32(delta_spin_neigh_w, delta_spin_neigh_s));
+            __m128i h_after = _mm_sub_epi32(h_before, _mm_slli_epi32(spin_new, 2));
 
-            __m256i delta_E = _mm256_sub_epi32(h_after, h_before);
-            __m256 mask1 = _mm256_cmp_ps(_mm256_set1_ps(temp), _mm256_setzero_ps(), _CMP_GT_OQ);
-            __m256 mask2 = _mm256_cmp_ps(_mm256_set1_ps(expf(-1 / temp)), _mm256_set1_ps(rand() / (float)RAND_MAX), _CMP_LE_OQ);
+            __m128i delta_E = _mm_sub_epi32(h_after, h_before);
+            __m128 p = _mm_div_ps(_mm_cvtepi32_ps(_mm_set1_epi32(rand())), _mm_set1_ps(RAND_MAX));
 
-            __m256i accept_mask = _mm256_or_si256(_mm256_cmpgt_epi32(delta_E, _mm256_setzero_si256()), (__m256i)mask2);
-            __m256i result = _mm256_blendv_epi32(spin_old, spin_new, accept_mask);
+            // Compare delta_E with zero and p with exp(-delta_E / temp)
+            __m128 accept_mask = _mm_or_ps(_mm_cmple_ps(_mm_setzero_ps(), _mm_cvtepi32_ps(delta_E)),
+                                            _mm_cmple_ps(p, _mm_exp_ps(_mm_div_ps(_mm_cvtepi32_ps(delta_E), _mm_set1_ps(temp)))));
 
-            _mm256_storeu_si256((__m256i*)&grid[i][j], result);
+            // Blend spin_old and spin_new based on accept_mask
+            __m128i result = _mm_blendv_epi8(spin_old, spin_new, _mm_castps_si128(accept_mask));
+
+            // Store the result back to memory
+            _mm_storeu_si128((__m128i*)&grid[i][j], result);
         }
     }
 }
+
 
 
 //Modificada
